127606645fc133cf98769a9f3b479ae6b27eace4
jmkim <wjdals0304@gmail.com>
2021-10-15 14:23:56 +0900

    Initial Commit

---
+++ TrendMediaProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrendMediaProject
+//
+//  Created by 김정민 on 2021/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
